f3dfcca59c934b15bd0c641cbad79fc008bc5241
marcschriftman <schrifty@gmail.com>
2013-09-22 13:11:51 -0500

    easier with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cycloserve::Application.config.secret_key_base = 'a531790b854b570bb25981b9d306640c65ffb56fa04bd615885d57a601b3cdb69efd7086d93d2a6f21a4d4c17e5c0a781a79a6fa4b604f4dcf1f831fec363ca2'
